f924b1c32437d6e4c94c65f87a56b9b3903637f3
EduardoMY <eduardo.medina@cetys.edu.mx>
2015-09-16 17:11:53 -0700

    Users helper deleted

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def hola
+  def lobby
 	
-    render text: "medina gay2!"
   end
 end
