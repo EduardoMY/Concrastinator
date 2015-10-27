74701ed9bc4e09fcde097e38d7be2d67ab9621b7
Daniel-Orozco <geoluisd@hotmail.com>
2015-10-26 20:59:53 -0700

    UI pass

--- blog/app/controllers/welcome_controller.rb
+++ blog/app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def home
+    
+  end
   def lobby
     render text: "Hello~! controllers/welcome_controller"
   end
--- blog/app/models/task.rb
+++ blog/app/models/task.rb
@@ -9,4 +9,11 @@ class Task < ActiveRecord::Base
   validates :priority_id, presence: true
   validates :due_date, presence: true
   validates :title, presence: true, length: { maximum: 50 }
+  validate :not_past_date
+
+	def not_past_date
+	  if self.due_date.past?
+	    errors.add(:due_date, 'not in past')
+	  end
+	end
 end
