8eed2768f920727d8a3026622c46660005eaa30f
Luis Orozco <mr.popcorner@hotmail.com>
2015-11-23 03:49:05 +0000

    profile pic size

--- blog/app/helpers/user_helper.rb
+++ blog/app/helpers/user_helper.rb
@@ -1,7 +1,7 @@
 module UserHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{220}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{200}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
